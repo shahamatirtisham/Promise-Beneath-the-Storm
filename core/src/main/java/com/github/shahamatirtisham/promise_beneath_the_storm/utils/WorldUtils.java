071f72c94549ee3dea0d98a0072fb7a1e7d59749
shahamatirtisham <shahamatirtisham22@gmail.com>
2026-07-15 23:43:11 +0600

    Load room metadata and collisions from Tiled

--- core/src/main/java/com/github/shahamatirtisham/promise_beneath_the_storm/utils/WorldUtils.java
+++ core/src/main/java/com/github/shahamatirtisham/promise_beneath_the_storm/utils/WorldUtils.java
@@ -2,6 +2,7 @@ package com.github.shahamatirtisham.promise_beneath_the_storm.utils;
 
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.math.Rectangle;
 
 public class WorldUtils {
 
@@ -10,6 +11,16 @@ public class WorldUtils {
         createWall(world, right, (bottom + top) / 2f, 0.1f, top - bottom);
         createWall(world, (left + right) / 2f, bottom, right - left, 0.1f);
         createWall(world, (left + right) / 2f, top, right - left, 0.1f);
+    }
+
+    public static void createStaticRectangle(World world, Rectangle rectangle) {
+        createWall(
+            world,
+            rectangle.x + rectangle.width / 2f,
+            rectangle.y + rectangle.height / 2f,
+            rectangle.width,
+            rectangle.height
+        );
     }
 
     private static void createWall(World world, float x, float y, float width, float height) {
