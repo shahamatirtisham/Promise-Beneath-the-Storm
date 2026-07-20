885bce022d1600842d975852c6716a6a1a1d69dc
shahamatirtisham <shahamatirtisham22@gmail.com>
2026-07-20 16:42:35 +0600

    Add chain-reacting explosive barrels

---
+++ core/src/main/java/com/github/shahamatirtisham/promise_beneath_the_storm/components/ExplosiveBarrelComponent.java
@@ -0,0 +1,15 @@
+package com.github.shahamatirtisham.promise_beneath_the_storm.components;
+
+import com.badlogic.ashley.core.Component;
+
+/** State for an attackable barrel that produces a short area explosion. */
+public class ExplosiveBarrelComponent implements Component {
+    public float health = 30f;
+    public float explosionRadius = 2.2f;
+    public float explosionDamage = 35f;
+    public float explosionTimeRemaining;
+    public float fuseTimeRemaining;
+    public boolean destroyed;
+    public boolean explosionApplied;
+    public int lastPlayerAttackId = -1;
+}
