2e3fee0c4bb6731dfc853ee84bdc55c2f5940709
shahamatirtisham <shahamatirtisham22@gmail.com>
2026-08-04 23:50:51 +0600

    Update project credits

--- core/src/main/java/com/github/shahamatirtisham/promise_beneath_the_storm/screens/CreditsScreen.java
+++ core/src/main/java/com/github/shahamatirtisham/promise_beneath_the_storm/screens/CreditsScreen.java
@@ -5,7 +5,7 @@ import com.github.shahamatirtisham.promise_beneath_the_storm.Main;
 public class CreditsScreen extends BaseMenuScreen {
     public CreditsScreen(final Main game) {
         super(game, "CREDITS");
-        addLabel("Game Design & Programming: Project Team");
+        addLabel("Game Design & Programming: Irtisham");
         addLabel("Sprites & Visuals: Arkam and Abtahi");
         addLabel("Built with Java, LibGDX, Ashley ECS, Box2D and Tiled");
         addButton("BACK", game::showMainMenu);
